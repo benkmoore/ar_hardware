1bcf15f6228b9c349e3f1aa5caa8d0918f2e1ca7
benkmoore <ben.karl.moore@gmail.com>
2020-07-06 15:18:23 +0100

    updated loading file for arduino

--- scripts/load_arduino_file.sh
+++ scripts/load_arduino_file.sh
@@ -6,11 +6,15 @@ if [ -z "$1" ]; then echo Must specify a file to compile and upload e.g. ./load_
 
 echo Arduino folder: "${file::-4}"
 
-~/arduino-1.8.12/arduino-builder -dump-prefs -logger=machine -hardware ~/arduino-1.8.12/hardware/ -tools ~/arduino-1.8.12/tools-builder -tools ~/arduino-1.8.12/hardware/tools/avr -built-in-libraries ~/arduino-1.8.12/libraries/ -libraries ~/Arduino/libraries -fqbn=teensy:avr:teensy40:usb=serial,speed=600,opt=o2std,keys=en-us -ide-version=10812 -build-path /tmp/arduino_build_733848/ -warnings=none -build-cache /tmp/arduino_cache_524788 -verbose ~/Arduino/"${file::-4}"/$1 
+mkdir /tmp/arduino_build
+mkdir /tmp/arduino_cache
 
-~/arduino-1.8.12/arduino-builder -compile -logger=machine -hardware ~/arduino-1.8.12/hardware/ -tools ~/arduino-1.8.12/tools-builder -tools ~/arduino-1.8.12/hardware/tools/avr -built-in-libraries ~/arduino-1.8.12/libraries/ -libraries ~/Arduino/libraries -fqbn=teensy:avr:teensy40:usb=serial,speed=600,opt=o2std,keys=en-us -ide-version=10812 -build-path /tmp/arduino_build_733848/ -warnings=none -build-cache /tmp/arduino_cache_524788 -verbose ~/Arduino/"${file::-4}"/$1 
 
-~/arduino-1.8.12/hardware/teensy/../tools/teensy_post_compile -file=$1 -path=/tmp/arduino_build_733848 -tools=/home/$USER/arduino-1.8.12/hardware/teensy/../tools -board=TEENSY40 -reboot -port=/sys/devices/70090000.xusb/usb1/1-2/1-2.4 -portlabel=/dev/bus/usb/001/011 Bootloader -portprotocol=Teensy
+~/arduino-1.8.12/arduino-builder -dump-prefs -logger=machine -hardware ~/arduino-1.8.12/hardware/ -tools ~/arduino-1.8.12/tools-builder -tools ~/arduino-1.8.12/hardware/tools/avr -built-in-libraries ~/arduino-1.8.12/libraries/ -libraries ~/Arduino/libraries -fqbn=teensy:avr:teensy40:usb=serial,speed=600,opt=o2std,keys=en-us -ide-version=10812 -build-path ~/tmp/arduino_build -warnings=none -build-cache /tmp/arduino_cache -verbose ~/Arduino/"${file::-4}"/$1 
+
+~/arduino-1.8.12/arduino-builder -compile -logger=machine -hardware ~/arduino-1.8.12/hardware/ -tools ~/arduino-1.8.12/tools-builder -tools ~/arduino-1.8.12/hardware/tools/avr -built-in-libraries ~/arduino-1.8.12/libraries/ -libraries ~/Arduino/libraries -fqbn=teensy:avr:teensy40:usb=serial,speed=600,opt=o2std,keys=en-us -ide-version=10812 -build-path /tmp/arduino_build -warnings=none -build-cache /tmp/arduino_cache -verbose ~/Arduino/"${file::-4}"/$1 
+
+~/arduino-1.8.12/hardware/teensy/../tools/teensy_post_compile -file=$1 -path=/tmp/arduino_build -tools=/home/$USER/arduino-1.8.12/hardware/teensy/../tools -board=TEENSY40 -reboot -port=/sys/devices/70090000.xusb/usb1/1-2/1-2.4 -portlabel=/dev/ttyACM0 Serial -portprotocol=Teensy
 
 
 
