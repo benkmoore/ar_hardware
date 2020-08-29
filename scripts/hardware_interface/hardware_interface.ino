22dca08c755b6ca61a064ef636eb6f8ccb50a216
benkmoore <ben.karl.moore@gmail.com>
2020-08-29 14:28:30 +0100

    testing stepper.h lib

--- scripts/hardware_interface/hardware_interface.ino
+++ scripts/hardware_interface/hardware_interface.ino
@@ -40,11 +40,15 @@ int DCMotorPins[N_DCMotors][3] = {{38, 37, 36}, {35, 34, 33}, {22, 21, 23}, {13,
 byte motorInterfaceType = 1;
 
 // Define steppers
-AccelStepper stepper1(motorInterfaceType, StepperPins[0][0], StepperPins[0][1]);
-AccelStepper stepper2(motorInterfaceType, StepperPins[1][0], StepperPins[1][1]);
-AccelStepper stepper3(motorInterfaceType, StepperPins[2][0], StepperPins[2][1]);
-AccelStepper stepper4(motorInterfaceType, StepperPins[3][0], StepperPins[3][1]);
-MultiStepper steppers;
+Stepper stepper1(int(360.0/PHI_STEP), StepperPins[0][0], StepperPins[0][1]);
+Stepper stepper2(int(360.0/PHI_STEP), StepperPins[1][0], StepperPins[1][1]);
+Stepper stepper3(int(360.0/PHI_STEP), StepperPins[2][0], StepperPins[2][1]);
+Stepper stepper4(int(360.0/PHI_STEP), StepperPins[3][0], StepperPins[3][1]);
+
+int phi_des1 = 0;
+int phi_des2 = 0;
+int phi_des3 = 0;
+int phi_des4 = 0;
 
 // Encoder pin outs
 int encPinA_1 = 26;                                   // enc1 -> stepper1
@@ -73,11 +77,11 @@ ros::NodeHandle_<ArduinoHardware, NUM_PUBS, NUM_SUBS, IN_BUFFER_SIZE, OUT_BUFFER
 // define ROS node name, rate, subscriber to /controller_cmds
 void controllerCmdCallback(const ar_commander::ControllerCmd &msg) {
   for(int i = 0; i < N_DCMotors; i++) {
-    if (msg.velocity_arr.data[i] > 0) {
-      Forward_DCMotor(msg.velocity_arr.data[i], DCMotorPins[i][0], DCMotorPins[i][1], DCMotorPins[i][2]);
+    if (msg.omega_arr.data[i] > 0) {
+      Forward_DCMotor(msg.omega_arr.data[i], DCMotorPins[i][0], DCMotorPins[i][1], DCMotorPins[i][2]);
     }
-    else if (msg.velocity_arr.data[i] < 0) {
-      Reverse_DCMotor(msg.velocity_arr.data[i], DCMotorPins[i][0], DCMotorPins[i][1], DCMotorPins[i][2]);
+    else if (msg.omega_arr.data[i] < 0) {
+      Reverse_DCMotor(msg.omega_arr.data[i], DCMotorPins[i][0], DCMotorPins[i][1], DCMotorPins[i][2]);
     }
     else {
       Forward_DCMotor(0, DCMotorPins[i][0], DCMotorPins[i][1], DCMotorPins[i][2]);
@@ -85,18 +89,18 @@ void controllerCmdCallback(const ar_commander::ControllerCmd &msg) {
   }
 
   // rads to degrees to int steps: (rad*(deg/rad) / (deg/step) = step
-  int phi_des1 = (int) ( (-msg.phi_arr.data[0]*RAD_2_DEG)/PHI_STEP );
-  int phi_des2 = (int) ( (-msg.phi_arr.data[1]*RAD_2_DEG)/PHI_STEP );
-  int phi_des3 = (int) ( (-msg.phi_arr.data[2]*RAD_2_DEG)/PHI_STEP );
-  int phi_des4 = (int) ( (-msg.phi_arr.data[3]*RAD_2_DEG)/PHI_STEP );
+  phi_des1 = (int) ( (-msg.phi_arr.data[0]*RAD_2_DEG)/PHI_STEP );
+  phi_des2 = (int) ( (-msg.phi_arr.data[1]*RAD_2_DEG)/PHI_STEP );
+  phi_des3 = (int) ( (-msg.phi_arr.data[2]*RAD_2_DEG)/PHI_STEP );
+  phi_des4 = (int) ( (-msg.phi_arr.data[3]*RAD_2_DEG)/PHI_STEP );
 
-  long pos[4];
-  pos[0] = phi_des1;
-  pos[1] = phi_des2;
-  pos[2] = phi_des3;
-  pos[3] = phi_des4;
-  steppers.moveTo(pos);
-  steppers.run();
+//  long pos[4];
+//  pos[0] = phi_des1;
+//  pos[1] = phi_des2;
+//  pos[2] = phi_des3;
+//  pos[3] = phi_des4;
+//  steppers.moveTo(pos);
+//  steppers.run();
 }
 
 ros::Subscriber<ar_commander::ControllerCmd> controller_cmds_sub("controller_cmds",controllerCmdCallback);
@@ -120,6 +124,37 @@ void Reverse_DCMotor(int PWMspeed, byte in1 , byte in2 , byte en) {
   analogWrite(en, PWMspeed);
 }
 
+// Calculate steps from encoder data pos to desired phi
+int calculateSteps(int encoder_data, int phi_des) {
+  int steps = phi_des - encoder_data;
+  if (abs(steps) > int(180.0/PHI_STEP)) {
+    if (steps > 0) {
+      steps = steps - int(360.0/PHI_STEP);
+    } else if (steps < 0) {
+      steps = steps + int(360.0/PHI_STEP);
+    }
+  }
+  return steps;
+}
+
+// Command number of steps (cw/ccw) for each stepper
+void commandSteppers(int enc1_pos, int enc2_pos, int enc3_pos, int enc4_pos) {
+  stepper1.step(calculateSteps(enc1_pos, phi_des1));
+  stepper2.step(calculateSteps(enc2_pos, phi_des2));
+  stepper3.step(calculateSteps(enc3_pos, phi_des3));
+  stepper4.step(calculateSteps(enc4_pos, phi_des4));
+}
+
+// wrap encoder output to [-100, 100] steps = [-pi, pi] rads
+int wrapToPi(float encoder_data) {
+  // counts * (degs/count) * (step/deg) = steps
+  int encoder_pos = int( (encoder_data)*(360.0/ENC_CPR)*(1.0/PHI_STEP) ) % int( 360.0/PHI_STEP );
+  if (encoder_pos > int( 180.0/PHI_STEP )) { encoder_pos = encoder_pos - int( 360.0/PHI_STEP ); }
+  else if (encoder_pos < int( -180.0/PHI_STEP )) { encoder_pos = encoder_pos + int( 360.0/PHI_STEP ); }
+
+  return encoder_pos;
+}
+
 /*
  * ------------- SETUP INTERFACE ------------------
  */
@@ -137,20 +172,11 @@ void setup() {
     pinMode(DCMotorPins[i][2], OUTPUT);
   }
 
-  stepper1.setMaxSpeed(MAX_STEPPER_VEL);
-  stepper2.setMaxSpeed(MAX_STEPPER_VEL);
-  stepper3.setMaxSpeed(MAX_STEPPER_VEL);
-  stepper4.setMaxSpeed(MAX_STEPPER_VEL);
-
-  stepper1.setCurrentPosition(0);
-  stepper2.setCurrentPosition(0);
-  stepper3.setCurrentPosition(0);
-  stepper4.setCurrentPosition(0);
-
-  steppers.addStepper(stepper1);
-  steppers.addStepper(stepper2);
-  steppers.addStepper(stepper3);
-  steppers.addStepper(stepper4);
+  stepper1.setSpeed(MAX_STEPPER_VEL);
+  stepper2.setSpeed(MAX_STEPPER_VEL);
+  stepper3.setSpeed(MAX_STEPPER_VEL);
+  stepper4.setSpeed(MAX_STEPPER_VEL);
+  
 }
 
 /*
@@ -160,24 +186,12 @@ void setup() {
 void loop() {
   hardware_interface.spinOnce();
 
-  // Feedback encoder data
-  // counts * (degs/count) * (step/deg) = steps
-  int enc1_pos = int( (enc1.read())*(360.0/ENC_CPR)*(1.0/PHI_STEP) ) % int( 180.0/PHI_STEP );
-  int enc2_pos = int( (enc2.read())*(360.0/ENC_CPR)*(1.0/PHI_STEP) ) % int( 180.0/PHI_STEP );
-  int enc3_pos = int( (enc3.read())*(360.0/ENC_CPR)*(1.0/PHI_STEP) ) % int( 180.0/PHI_STEP );
-  int enc4_pos = int( (enc4.read())*(360.0/ENC_CPR)*(1.0/PHI_STEP) ) % int( 180.0/PHI_STEP );
-
-  if (abs(-enc1_pos-stepper1.currentPosition()) > DEADBAND) {
-    stepper1.setCurrentPosition(-enc1_pos);
-  }
-  if (abs(-enc2_pos-stepper2.currentPosition()) > DEADBAND) {
-    stepper2.setCurrentPosition(-enc2_pos);
-  }
-  if (abs(-enc3_pos-stepper3.currentPosition()) > DEADBAND) {
-    stepper3.setCurrentPosition(-enc3_pos);
-  }
-  if (abs(-enc4_pos-stepper4.currentPosition()) > DEADBAND) {
-    stepper4.setCurrentPosition(-enc4_pos);
-  }
+  // Feedback encoder data & wrap to [-pi, pi] = [-100, 100] steps
+  int enc1_pos = wrapToPi(enc1.read());
+  int enc2_pos = wrapToPi(enc2.read());
+  int enc3_pos = wrapToPi(enc3.read());
+  int enc4_pos = wrapToPi(enc4.read());
+  
+  commandSteppers(enc1_pos, enc2_pos, enc3_pos, enc4_pos);
 
 }
