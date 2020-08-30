60583cf719123ce49b5da66dbcea1cee06c42343
benkmoore <ben.karl.moore@gmail.com>
2020-08-30 16:11:20 +0100

    tested on hardware

--- scripts/rebuild_hardware_stack.sh
+++ scripts/rebuild_hardware_stack.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# rebuild workspace
 ( cd ~/catkin_ws; catkin_make )
 
+# rosserial: remove old ros msgs and rebuild new msgs
 rm -rf ~/Arduino/libraries/ros_lib;
-rosrun rosserial_arduino make_libraries.py ~/Arduino/libraries ~/catkin_ws/src/ar_commander/msg
+rosrun rosserial_arduino make_libraries.py ~/Arduino/libraries ~/catkin_ws/src/ar_commander/msg;
