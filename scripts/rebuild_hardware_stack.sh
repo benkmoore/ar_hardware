9f87e3b714862e83d5eff6480e7b5e64de1ead3b
benkmoore <ben.karl.moore@gmail.com>
2020-08-30 16:15:40 +0100

    load teensy added

--- scripts/rebuild_hardware_stack.sh
+++ scripts/rebuild_hardware_stack.sh
@@ -6,3 +6,6 @@
 # rosserial: remove old ros msgs and rebuild new msgs
 rm -rf ~/Arduino/libraries/ros_lib;
 rosrun rosserial_arduino make_libraries.py ~/Arduino/libraries ~/catkin_ws/src/ar_commander/msg;
+
+# reload teensy with new msgs
+./load_arduino_file.sh teensy41 hardware_interface.ino;
