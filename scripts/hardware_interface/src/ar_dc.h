f3274b3b219d9e69a7042c7fef7adb951502bfcd
kevin <kevinoflannagain@gmail.com>
2020-09-29 13:07:54 +0100

    no longer hard coding array size for flipping direction of wheels

--- scripts/hardware_interface/src/ar_dc.h
+++ scripts/hardware_interface/src/ar_dc.h
@@ -3,17 +3,14 @@ class DC_Motors{
   public:
 
     DC_Motors(int* reverseFlags, int* DC_reverse, int* DC_throttlepins, int N_DCMotors);
-
     void PowerDC(int aPin, int PWMspeed, int index);
-
-    void flipDirection();
-    int flip[4] = {0,0,0,0};
+    void flipDirection();    
     int flipFlag = 0;
     int N_DCMotors;
+    
   private:
     int *reverse;
     int *throttlePins;
     int *reverseFlags;
-
-
+    int flip[N_DCMotors] = {0};
 };
