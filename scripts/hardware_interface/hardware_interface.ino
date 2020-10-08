cdd4a47b62fd7aa456aa7f8bd5568ea63e02a113
benkmoore <ben.karl.moore@gmail.com>
2020-10-08 17:44:13 +0100

    test code for new dc driving

--- scripts/hardware_interface/hardware_interface.ino
+++ scripts/hardware_interface/hardware_interface.ino
@@ -4,7 +4,12 @@
 #include "Encoder.h"
 #include "SPI.h"
 #include "src/ar_stepper.h"
-#include "src/ar_dc.h"
+//#include "src/ar_dc.h"
+# include "Wire.h"
+#include <Adafruit_MCP4728.h>
+//#include "iostream"
+//#include <string>
+//using namespace std;
 
 #include <std_msgs/Float64.h>
 #define RX        7
@@ -48,16 +53,16 @@ const int N_StepperMotors = 4;
 // Step Motor pins: outer Y axis arm, inner Y axis arm, inner X axis arm, outer X axis arm
 int StepperMotorPins[N_StepperMotors] = {10, 36, 37, 38};
 
-char channel[4] = {'MCP4728_CHANNEL_A','MCP4728_CHANNEL_B','MCP4728_CHANNEL_C', 'MCP4728_CHANNEL_D','\0'};
 
 // DC Motor pins
 int DC_reverse[N_DCMotors] = {20, 21, 22, 23};
 // analog pins A0 to A3 correspond to pins 14, 15 and 28, 29 on the teensy
 int DC_throttlePins[N_DCMotors] = {A0, A1, 28, 29};
 
-int reverseFlags[N_DCMotors] = {0, 0, 0, 0};
+int DC_reverseFlags[N_DCMotors] = {0, 0, 0, 0};
 int flip[N_DCMotors] = {0, 0, 0, 0};
-
+int flipFlag = 0;
+//std::string channel[4] = {"MCP4728_CHANNEL_A", "MCP4728_CHANNEL_B", "MCP4728_CHANNEL_C", "MCP4728_CHANNEL_D"};
 
 // Define steppers
 Stepper stepper1(int(360.0 / PHI_STEP), PHI_STEP, STEPS_THRESHOLD, MAX_STEPPER_VEL, MIN_STEPPER_VEL, MAX_MILLIAMPS, MICRO_STEP_SIZE, DECAY_MODE);
@@ -65,9 +70,9 @@ Stepper stepper2(int(360.0 / PHI_STEP), PHI_STEP, STEPS_THRESHOLD, MAX_STEPPER_V
 Stepper stepper3(int(360.0 / PHI_STEP), PHI_STEP, STEPS_THRESHOLD, MAX_STEPPER_VEL, MIN_STEPPER_VEL, MAX_MILLIAMPS, MICRO_STEP_SIZE, DECAY_MODE);
 Stepper stepper4(int(360.0 / PHI_STEP), PHI_STEP, STEPS_THRESHOLD, MAX_STEPPER_VEL, MIN_STEPPER_VEL, MAX_MILLIAMPS, MICRO_STEP_SIZE, DECAY_MODE);
 
-DC_Motors DC_motors(reverseFlags, DC_reverse, DC_throttlePins, N_DCMotors, flip);
+//DC_Motors DC_motors(DC_reverseFlags, DC_reverse, DC_throttlePins, N_DCMotors, flip);
 AMTEncoder encoder(Re, De);
-
+Adafruit_MCP4728 mcp;
 int phi_des1 = 0;
 int phi_des2 = 0;
 int phi_des3 = 0;
@@ -88,25 +93,63 @@ ros::NodeHandle_<ArduinoHardware, NUM_PUBS, NUM_SUBS, IN_BUFFER_SIZE, OUT_BUFFER
 // define ROS node name, rate, subscriber to /controller_cmds
 void controllerCmdCallback(const ar_commander::ControllerCmd &msg) {
   for (int i = 0; i < N_DCMotors; i++) {
-    if (msg.omega_arr.data[i] > 0){
-     // pwmVal =  map(msg.omega_arr.data[i], MIN_VEL, MAX_VEL, MIN_PWM, MAX_PWM);
-       pwmVal = 90;
+    if (msg.omega_arr.data[i] > 0) {
+      pwmVal =  map(msg.omega_arr.data[i], MIN_VEL, MAX_VEL, MIN_PWM, MAX_PWM);
+
     }
-    else if (msg.omega_arr.data[i] < 0){
-      //pwmVal =  map(msg.omega_arr.data[i], -1*MAX_VEL, MIN_VEL, -1*MAX_PWM, -1*MIN_PWM);
-      pwmVal = -90;    
-} 
-    else{
+    else if (msg.omega_arr.data[i] < 0) {
+      pwmVal =  map(msg.omega_arr.data[i], -1 * MAX_VEL, MIN_VEL, -1 * MAX_PWM, -1 * MIN_PWM);
+
+    }
+    else {
       pwmVal = 0;
     }
 
-    DC_motors.PowerDC(DC_throttlePins[i], pwmVal, i);
+    if (pwmVal > 0 ) {
+      if (DC_reverseFlags[i] == 0) {
+        // analogWrite(aPin, pwmVal);
+        mcp.setChannelValue(MCP4728_CHANNEL_A, pwmVal);
+      }
+      else {
+        flip[i] = 1;
+        flipFlag = 1;
+      }
+    }
+
+    else if (pwmVal < 0) {
+      if (DC_reverseFlags[i] == 1) {
+        // analogWrite(aPin, pwmVal*-1);
+        mcp.setChannelValue(MCP4728_CHANNEL_A, pwmVal);
+
+      }
+      else {
+        flip[i] = 1;
+        flipFlag = 1;
+      }
+    }
+    else if (pwmVal == 0 ) {
+      // analogWrite(aPin, pwmVal);
+      mcp.setChannelValue(MCP4728_CHANNEL_A, pwmVal);
+
+    }
+
   }
 
-  if (DC_motors.flipFlag == 1){
-    DC_motors.flipDirection();
+  if (flipFlag == 1) {
+    for (int i = 0; i < N_DCMotors; i++) {
+      if (flip[i] == 1) {
+        digitalWrite(DC_reverse[i], LOW);
+        flip[i] = 0;
+        DC_reverseFlags[i] = !DC_reverseFlags[i];
+      }
+    }
+    flipFlag = 0;
+    delay(500);
+    for (int i = 0; i < N_DCMotors; i++) {
+      digitalWrite(DC_reverse[i], HIGH);
+    }
   }
-  
+
 
   // rads to degrees to int steps: (rad*(deg/rad) / (deg/step) = step
   phi_des1 = (int) ( (msg.phi_arr.data[0] * RAD_2_DEG) / PHI_STEP );
@@ -149,6 +192,8 @@ void setup() {
   hardware_interface.initNode();
   hardware_interface.advertise(chatter_pub);
   hardware_interface.subscribe(controller_cmds_sub);
+  delay (5000);
+  mcp.begin();
 
   // Setup stepper motors
   SPI.begin();
@@ -160,14 +205,14 @@ void setup() {
   pinMode(A1, OUTPUT);
   pinMode(28, OUTPUT);
   pinMode(29, OUTPUT);
-  // Setup DC reverse pins
+  // Setup DC DC_reverse pins
   for (int i = 0; i < N_DCMotors; i++) {
     pinMode(DC_reverse[i], OUTPUT);
     digitalWrite(DC_reverse[i], HIGH);
   }
   pinMode(Re, OUTPUT);
-  pinMode(De, OUTPUT);      
-//  Serial.begin(57600);
+  pinMode(De, OUTPUT);
+  //  Serial.begin(57600);
 
 }
 
@@ -175,29 +220,30 @@ void setup() {
    ------------- MAIN ------------------
 */
 void loop() {
- // hardware_interface.spinOnce();
+  // hardware_interface.spinOnce();
   // int out_76 = wrapToPi(encoder.checkEncoder(76));
   // test.data = out_76;
   // chatter_pub.publish(&test);
-/*if((abs(wrapToPi(encoder.checkEncoder(76)))-phi_des1)>DEADBAND){
-stepper1.commandStepper(wrapToPi(encoder.checkEncoder(76)), phi_des1);
-}
-if((abs(wrapToPi(encoder.checkEncoder(80)))-phi_des2)>DEADBAND){
- stepper1.commandStepper(wrapToPi(encoder.checkEncoder(80)), phi_des1);
-}
-if((abs(wrapToPi(encoder.checkEncoder(84)))-phi_des3)>DEADBAND){
- stepper1.commandStepper(wrapToPi(encoder.checkEncoder(84)), phi_des1);
-}
-if((abs(wrapToPi(encoder.checkEncoder(88))))-phi_des4>DEADBAND){
- stepper1.commandStepper(wrapToPi(encoder.checkEncoder(88)), phi_des1);
-}*/
- // Feedback encoder data & wrap to [-pi, pi] = [-100, 99] steps
+  /*if((abs(wrapToPi(encoder.checkEncoder(76)))-phi_des1)>DEADBAND){
+    stepper1.commandStepper(wrapToPi(encoder.checkEncoder(76)), phi_des1);
+    }
+    if((abs(wrapToPi(encoder.checkEncoder(80)))-phi_des2)>DEADBAND){
+    stepper1.commandStepper(wrapToPi(encoder.checkEncoder(80)), phi_des1);
+    }
+    if((abs(wrapToPi(encoder.checkEncoder(84)))-phi_des3)>DEADBAND){
+    stepper1.commandStepper(wrapToPi(encoder.checkEncoder(84)), phi_des1);
+    }
+    if((abs(wrapToPi(encoder.checkEncoder(88))))-phi_des4>DEADBAND){
+    stepper1.commandStepper(wrapToPi(encoder.checkEncoder(88)), phi_des1);
+    }*/
+  // Feedback encoder data & wrap to [-pi, pi] = [-100, 99] steps
   stepper1.commandStepper(wrapToPi(encoder.checkEncoder(76)), phi_des1);
   stepper2.commandStepper(wrapToPi(encoder.checkEncoder(80)), phi_des2);
   stepper3.commandStepper(wrapToPi(encoder.checkEncoder(84)), phi_des3);
   stepper4.commandStepper(wrapToPi(encoder.checkEncoder(88)), phi_des4);
-  if (millis()-callbackTime>1000){
-    for (int i = 0; i < N_DCMotors; i++){ 
-      DC_motors.PowerDC(DC_throttlePins[i], 0, i);
-}}
+  //  if (millis()-callbackTime>1000){
+  //    for (int i = 0; i < N_DCMotors; i++){
+  //      DC_motors.PowerDC(channel[i], 0, i);
+  //}
+//}
 }
