d635d38a44cabc646837c7ee45c4ed7a0f1e7dd1
kevin oflannagain <kevinoflannagain@gmail.com>
2020-10-14 22:43:30 +0100

    transforms for y arm and x arm tofs working

--- scripts/tof_tests/tof_data_sim.py
+++ scripts/tof_tests/tof_data_sim.py
@@ -12,19 +12,19 @@ def tofSensors():
     rate = rospy.Rate(10) # 10hz
     pubTOF = rospy.Publisher('tof_data', TOF, queue_size=10)
     pubState = rospy.Publisher('estimator/state', State, queue_size=10)
-    print 'started'
+    print ('started')
 
     while not rospy.is_shutdown():
         # tof = np.random.randint(1,100, size=(1,3))
 
         state.pos.data = np.zeros(2)
-        state.theta.data = 0#np.pi/4
+        state.theta.data = 0
         state.vel.data = np.zeros(2)
         state.omega.data = 0
 
-        #tof.tof1 = np.random.randint(50,200)
-        tof.tof2 = 1000#np.random.randint(250,400)
-        #tof.tof3 = np.random.randint(50,200)
+        tof.tof1 = 500#np.random.randint(50,200)
+        tof.tof2 = 500#np.random.randint(250,400)
+        tof.tof3 = 500#np.random.randint(50,200)
         rospy.loginfo_throttle(3, tof)
 
 
--- scripts/tof_tests/xy_data.py
+++ scripts/tof_tests/xy_data.py
@@ -13,16 +13,15 @@ class SensorNode():
     
     def __init__(self):
         rospy.init_node('XY_data', anonymous=True)
-
-        self.posLeft = [0,400]
-        self.posMid = [0,0]
-        self.posRight = [400,0]
+        self.posLeft = None
+        self.posMid = None
+        self.posRight = None
         self.angleL = 0
         self.angleM = np.pi/4
         self.angleR = np.pi/2
-        self.xyLeft = [0,0]
-        self.xyMid = [0,0]
-        self.xyRight = [0,0]
+        self.xyLeft = None
+        self.xyMid = None
+        self.xyRight = None
         self.theta = 0
         # print self.xyLeft, self.xyMid, self.xyRight
 
@@ -31,18 +30,30 @@ class SensorNode():
         self.vel = np.array(msg.vel.data)
         self.theta = msg.theta.data
         self.omega = msg.omega.data
-        #self.posLeft = self.pos*1000 + [150,450]
-        self.posMid = self.pos*1000 + [150,150]
-        #self.posRight = self.pos*1000 + [450,150]
+
+        sinM = np.sin((self.theta+np.pi/4))
+        cosM = np.cos((self.theta+np.pi/4))
+        sinL = np.sin((self.theta+1.249))
+        cosL = np.cos((self.theta+1.249))
+        sinR = np.sin((self.theta+0.322))
+        cosR = np.cos((self.theta+0.322))
+
+        self.absLeft = np.sqrt(150**2+450**2)
+        self.absMid = np.sqrt(150**2+150**2)
+        self.absRight = np.sqrt(450**2+150**2)
+
+        self.posLeft = self.pos*1000 + [cosL*self.absLeft,sinL*self.absLeft]
+        self.posMid = self.pos*1000 + [cosM*self.absMid,sinM*self.absMid]
+        self.posRight = self.pos*1000 + [cosR*self.absRight,sinR*self.absRight]
 
 
     def xyCallback(self, tof_data):
-        sinMid = np.sin((self.angleM - self.theta))
-        cosMid = np.cos((self.angleM - self.theta))
-        sinLeft = np.sin((self.angleL - self.theta))
-        cosLeft = np.cos((self.angleL - self.theta))
-        sinRight = np.sin((self.angleR - self.theta))
-        cosRight = np.cos((self.angleR - self.theta))
+        sinMid = np.sin((self.angleM + self.theta))
+        cosMid = np.cos((self.angleM + self.theta))
+        sinLeft = np.sin((self.angleL + self.theta))
+        cosLeft = np.cos((self.angleL + self.theta))
+        sinRight = np.sin((self.angleR + self.theta))
+        cosRight = np.cos((self.angleR + self.theta))
 
             #left refers to the sensor on the y axis, right is the sensor on the x 
             #when robot corner is at 0,0 left arm goes from (0,0) to (0,450) in mm
@@ -51,10 +62,10 @@ class SensorNode():
         mid_tof = tof_data.tof2
         right_tof = tof_data.tof3
 
-        #self.xyLeft = [round(left_tof*cosLeft + self.posLeft[0],2), round(left_tof*sinLeft + self.posLeft[1],2)]
-        self.xyMid = [round(mid_tof*sinMid + self.posMid[0],2), round(mid_tof*cosMid + self.posMid[1],2)]
-        #self.xyRight = [round(right_tof*cosRight + self.posRight[0],2), round(right_tof*sinRight + self.posRight[1],2)]
-        # print self.xyLeft, self.xyMid, self.xyRight
+        if self.posLeft is not None:
+            self.xyLeft = [round(left_tof*cosLeft + self.posLeft[0],2), round(left_tof*sinLeft + self.posLeft[1],2)]
+            self.xyMid = [round(mid_tof*cosMid + self.posMid[0],2), round(mid_tof*sinMid + self.posMid[1],2)]
+            self.xyRight = [round(right_tof*cosRight + self.posRight[0],2), round(right_tof*sinRight + self.posRight[1],2)]
 
     def main(self):
         rospy.Subscriber('tof_data', TOF, self.xyCallback)
@@ -62,17 +73,22 @@ class SensorNode():
 
         self.rate = float(rospy.get_param('~rate', 70.0))
         rate = rospy.Rate(self.rate)
-
+      
         while not rospy.is_shutdown(): #and self.xyLeft != None:
-                #plt.scatter(self.xyLeft[0],self.xyLeft[1])
+            if self.xyLeft is not None:
+                plt.scatter(self.xyLeft[0],self.xyLeft[1],marker = ".")
                 plt.scatter(self.xyMid[0],self.xyMid[1],marker = ".")        
-                #plt.scatter(self.xyRight[0],self.xyRight[1])
+                plt.scatter(self.xyRight[0],self.xyRight[1],marker = ".")
+
+                plt.scatter(self.posLeft[0], self.posLeft[1], c = "g", marker = "x")
+                plt.scatter(self.posMid[0], self.posMid[1], c = "g", marker = "x")        
+                plt.scatter(self.posRight[0], self.posRight[1], c = "g", marker = "x")
+
                 plt.xlabel("x value in mm")
                 plt.ylabel("y value in mm")
-                # i+=1;
                 plt.show()
                 plt.pause(0.0001)
-                rate.sleep()
+            rate.sleep()
 
 #                print self.posMid, self.xyMid, self.theta
         rospy.spin()
@@ -83,8 +99,9 @@ class SensorNode():
 if __name__ == '__main__':
     try:
         plt.ion()
+       
         fig=plt.figure()
-        plt.axis([-2000,6000,-2000,6000])
+        plt.axis([-1000,1000,-1000,1000])
 
         sensors = SensorNode()
         sensors.main()
