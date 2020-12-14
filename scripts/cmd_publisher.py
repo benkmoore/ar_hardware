73a806aee75b443384dd7debc93d6e86bc0a24bc
kevin oflannagain <kevinoflannagain@gmail.com>
2020-12-14 13:10:38 +0000

    classified cmd publisher

--- scripts/cmd_publisher.py
+++ scripts/cmd_publisher.py
@@ -5,51 +5,98 @@ Script publish the same cmd to multiple robots
 """
 
 import numpy as np
+import time
 import rospy
 import rosnode
+
 from std_msgs.msg import Int8, Bool
 from ar_commander.msg import ControllerCmd
 
 
 RATE = 70
-
-Omega = np.ones(4)*0.4
-Phi = np.ones(4)*0#.15
-
-rospy.init_node('cmd_publisher')
-
-pub_cmds_1 = rospy.Publisher('/robot1/controller_cmds', ControllerCmd, queue_size=10)
-pub_cmds_2 = rospy.Publisher('/robot2/controller_cmds', ControllerCmd, queue_size=10)
-pub_cmds_3 = rospy.Publisher('/robot3/controller_cmds', ControllerCmd, queue_size=10)
-pub_cmds_4 = rospy.Publisher('/robot4/controller_cmds', ControllerCmd, queue_size=10)
-
-def publish():
-        cmd1 = ControllerCmd()
-        cmd2 = ControllerCmd()
-        cmd3 = ControllerCmd()
-        cmd4 = ControllerCmd()
-
-        cmd1.omega_arr.data = Omega
-        cmd1.phi_arr.data = Phi + np.pi/2# - 2*Phi
-        cmd2.omega_arr.data = Omega
-        cmd2.phi_arr.data = Phi
-        cmd3.omega_arr.data = Omega
-        cmd3.phi_arr.data = Phi
-        cmd4.omega_arr.data = Omega
-        cmd4.phi_arr.data = Phi #+ np.pi/2
-
-        pub_cmds_1.publish(cmd1)
-        pub_cmds_2.publish(cmd2)
-        pub_cmds_3.publish(cmd3)
-        pub_cmds_4.publish(cmd4)
+OMEGA = 0.4
 
 
-def run():
-    rate = rospy.Rate(RATE) # 10 Hz
-    while not rospy.is_shutdown():
-        publish()
-        rate.sleep()
+class Commander():
+
+    def __init__(self):
+        rospy.init_node('cmd_publisher')
+
+        self.pub_cmds_1 = rospy.Publisher('/robot1/controller_cmds', ControllerCmd, queue_size=10)
+        self.pub_cmds_2 = rospy.Publisher('/robot2/controller_cmds', ControllerCmd, queue_size=10)
+        self.pub_cmds_3 = rospy.Publisher('/robot3/controller_cmds', ControllerCmd, queue_size=10)
+        self.pub_cmds_4 = rospy.Publisher('/robot4/controller_cmds', ControllerCmd, queue_size=10)
+
+        self.cmd1 = ControllerCmd()
+        self.cmd2 = ControllerCmd()
+        self.cmd3 = ControllerCmd()
+        self.cmd4 = ControllerCmd()
+
+        self.start = time.time()
+
+        self.Omega = np.ones(4)*OMEGA
+        self.Phi = np.ones(4)*0#.15
+
+        self.cmd1.omega_arr.data = self.Omega
+        self.cmd1.phi_arr.data = self.Phi #+ np.pi/2# - 2*Phi
+        self.cmd2.omega_arr.data = self.Omega
+        self.cmd2.phi_arr.data = self.Phi
+        self.cmd3.omega_arr.data = self.Omega
+        self.cmd3.phi_arr.data = self.Phi
+        self.cmd4.omega_arr.data = self.Omega
+        self.cmd4.phi_arr.data = self.Phi
+
+
+    def publish(self):
+        self.pub_cmds_1.publish(self.cmd1)
+        self.pub_cmds_2.publish(self.cmd2)
+        self.pub_cmds_3.publish(self.cmd3)
+        self.pub_cmds_4.publish(self.cmd4)
+
+    def spin(self):
+        self.Omega = np.ones(4)*OMEGA
+        self.Phi = np.ones(4)*(-np.pi/6)#.15
+
+        self.cmd1.omega_arr.data = self.Omega
+        self.cmd1.phi_arr.data = self.Phi #+ np.pi/2# - 2*Phi
+        self.cmd2.omega_arr.data = self.Omega
+        self.cmd2.phi_arr.data = self.Phi
+        self.cmd3.omega_arr.data = self.Omega
+        self.cmd3.phi_arr.data = self.Phi
+        self.cmd4.omega_arr.data = self.Omega
+        self.cmd4.phi_arr.data = self.Phi #+ np.pi/2
+
+    def straight(self):
+        self.Phi = np.ones(4)*0#.15
+        self.cmd1.omega_arr.data = self.Omega
+        self.cmd1.phi_arr.data = self.Phi + np.pi/2# - 2*Phi
+        self.cmd2.omega_arr.data = self.Omega
+        self.cmd2.phi_arr.data = self.Phi 
+        self.cmd3.omega_arr.data = self.Omega
+        self.cmd3.phi_arr.data = self.Phi + np.pi
+        self.cmd4.omega_arr.data = self.Omega
+        self.cmd4.phi_arr.data = self.Phi 
+
+    def reverse(self):
+        self.straight()
+        self.Phi -= np.pi
+
+    def U(self):
+        self.straight()
+        if(time.time() - self.start) > 4:
+            self.reverse()
+        elif(time.time() - self.start) > 2:
+            self.Omega -= np.pi/2
+        
+
+    def run(self):
+        rate = rospy.Rate(RATE) 
+        while not rospy.is_shutdown():
+            self.U()
+            self.publish()
+            rate.sleep()
 
 
 if __name__ == '__main__':
-    run()
+    commander = Commander()
+    commander.run()
