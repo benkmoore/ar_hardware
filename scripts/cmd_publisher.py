341aa7b9219bfe11b7e1e91fdc4914e0ae71b2c4
kevin oflannagain <kevinoflannagain@gmail.com>
2020-12-14 11:48:25 +0000

    changed offset

--- scripts/cmd_publisher.py
+++ scripts/cmd_publisher.py
@@ -36,7 +36,7 @@ def publish():
         cmd3.omega_arr.data = Omega
         cmd3.phi_arr.data = Phi
         cmd4.omega_arr.data = Omega
-        cmd4.phi_arr.data = Phi + np.pi/2
+        cmd4.phi_arr.data = Phi #+ np.pi/2
 
         pub_cmds_1.publish(cmd1)
         pub_cmds_2.publish(cmd2)
